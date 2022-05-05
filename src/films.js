906d78371653efbdf89f8524de2f3433023e7751
Joel <jsboquet@gmail.com>
2022-05-05 17:31:49 +0200

    Exercici 2

--- src/films.js
+++ src/films.js
@@ -5,7 +5,10 @@ function getAllDirectors(array) {
 }
 
 // Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {}
+function getMoviesFromDirector(array, director) {
+  let moviesFromDirector = array.filter((item) => item.director === director);
+  return moviesFromDirector;
+}
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {}
