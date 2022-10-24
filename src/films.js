bd23b954c3944e5e33de53767adbcccdcd974014
Martí Arnau <marnauortega@gmail.com>
2022-10-24 16:46:56 +0200

    n1 - exercice 1

--- src/films.js
+++ src/films.js
@@ -1,7 +1,6 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+  let result = array.map((el) => el.director);
   return result;
 }
 
