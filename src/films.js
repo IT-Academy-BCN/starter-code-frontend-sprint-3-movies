9e49981bd1e30b630cd106c3af3e2a3ab43ba460
Martí Arnau <marnauortega@gmail.com>
2022-10-24 16:47:19 +0200

    n1 - exercice 2

--- src/films.js
+++ src/films.js
@@ -6,7 +6,8 @@ function getAllDirectors(array) {
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let result = array.filter((el) => el.director === director);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
