5fe058e6e4c710dbebe36bb6810a316c3fba35b2
Ana María Águila <ana_mari_aguila@hotmail.com>
2022-12-01 08:57:13 +0100

    Ej2. Muestra películas director

--- src/films.js
+++ src/films.js
@@ -8,7 +8,10 @@ function getAllDirectors(array) {
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  const result = array.filter(element => element.director === director);
+
+  console.log("EXERCICE 2 ->", result);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
