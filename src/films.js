0abd093849d8435c723460866bf346d4f6a43c32
jenDevelopez <jen1992lo@gmail.com>
2023-05-23 09:31:18 +0200

    Ejercicio 8 terminado

--- src/films.js
+++ src/films.js
@@ -40,7 +40,7 @@ function orderByYear(array) {
 };
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
+function moviesAverageByCategory(array,genre) {
   let movieGenre = array.filter(movie => movie.genre.includes(genre)).map(movie => movie.score)
   let valids = movieGenre.filter(value => typeof value === 'number');
   let totalScore = valids.reduce((acc,sig) => acc + sig)
@@ -51,28 +51,23 @@ function moviesAverageByCategory() {
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes(array){
-  let result = []
-  let hours = 0
-  let minuts = 0
-  let minutsMovie = 0
-  //Itero el array de peliculas
-  for(let movie of movies){
-    let duration = movie.duration
-    hours = parseInt(duration[0])
-    minuts = parseInt(duration.slice(3,5))
-    minutsMovie = (hours * 60) + minuts
-    movie.duration = minutsMovie
-    result.push(movie)
-  }
-  return result
+  return  array.map(movie => {
+    const duration = movie.duration;
+    const [hoursString, minutesString] = duration.split('h').map(time => time.trim());
+    const hours = parseInt(hoursString)
+    const minutes = minutesString ? parseInt(minutesString) : 0;
+    const totalMinutes = (hours * 60) + minutes;
+    return { ...movie, duration: totalMinutes } ;
+  });
 }
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
-  
+function bestFilmOfYear(array,year) {
+  let result = []
+  let bestMovie = array.filter(movie => movie.year === year).reduce((previusFilm,currentFilm) => currentFilm.score > previusFilm.score ? currentFilm : previusFilm)
+  result.push(bestMovie)
+  return result
 }
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
