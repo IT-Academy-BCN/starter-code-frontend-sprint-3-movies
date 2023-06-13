3ad561cc9688a8d167e3709a50d0a48a3c5043af
PamelaSCB <pamiboyle@gmail.com>
2023-06-13 19:30:01 +0200

    Ejercicio 4-Nivel 1

--- src/films.js
+++ src/films.js
@@ -25,7 +25,14 @@ function moviesAverageOfDirector(array, director) {
 }
 
 // Exercise 4:  Alphabetic order by title
-function orderAlphabetically(array) {}
+function orderAlphabetically(array) {
+  const result = array.map(element => element.title).sort();
+  if (result.length>20) {
+   result.length=20;  
+  } 
+  console.log('EXERCISE 4 ->', result);
+  return result;
+}
 
 // Exercise 5: Order by year, ascending
 function orderByYear() {}
