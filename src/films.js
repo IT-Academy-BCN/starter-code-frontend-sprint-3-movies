4edb8da8c9bd34d481f5ba9432b910a62b6f5828
VladScv <ddcllrnsbrv@uoc.edu>
2023-06-13 12:41:00 +0200

    Exercise 2

--- src/films.js
+++ src/films.js
@@ -10,7 +10,9 @@ function getAllDirectors(array) {
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  const result = array.filter(movie => movie.director === director);
+  console.log("EXERCICE 2 ->", result);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
