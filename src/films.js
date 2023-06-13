a6d9301c94ec8dcfa62e0cc9bb8596b8d5e4c51c
PamelaSCB <pamiboyle@gmail.com>
2023-06-13 18:27:02 +0200

    Ejercicio 2- Nivel 1

--- src/films.js
+++ src/films.js
@@ -7,6 +7,9 @@ function getAllDirectors(array) {
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
+const result= array.filter(element=> element.director=== director); 
+console.log('EXERCISE 2 ->', result);
+return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
