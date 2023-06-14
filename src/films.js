881c6de6dd8c75404c740f289c418f9060249e77
VladScv <ddcllrnsbrv@uoc.edu>
2023-06-14 12:19:35 +0200

    Exercises 2 and 3

--- src/films.js
+++ src/films.js
@@ -1,28 +1,29 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  [];
-  for (let movie of array) {
+  const result =  [];
+  array.map (movie => {
     if(result.length===0 || result.indexOf(movie.director)<0) result.push(movie.director)
-  }
-  console.log("EXERCICE 1 ->", result);
+  })
+  console.log("EXERCISE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
   const result = array.filter(movie => movie.director === director);
-  console.log("EXERCICE 2 ->", result);
+  console.log("EXERCISE 2 ->", result);
   return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  const dirMovies = getMoviesFromDirector(array,director);
+  return dirMovies.reduce((res,film)=>res + parseFloat(film.score),0)/dirMovies.length
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+   return array.map(el=>el.title).sort().slice(0,20);
 }
 
 // Exercise 5: Order by year, ascending
