e4ff5c086e32c4080fbd8efa6bd2cb9688171a1e
VladScv <ddcllrnsbrv@uoc.edu>
2023-06-14 10:41:17 +0200

    Added babel configuration

---
+++ output/films.js
@@ -0,0 +1,65 @@
+"use strict";
+
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+// Exercise 1: Get the array of all directors.
+function getAllDirectors(array) {
+  var result = [];
+  var _iterator = _createForOfIteratorHelper(array),
+    _step;
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var movie = _step.value;
+      if (result.length === 0 || result.indexOf(movie.director) < 0) result.push(movie.director);
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+  console.log("EXERCICE 1 ->", result);
+  return result;
+}
+
+// Exercise 2: Get the films of a certain director
+function getMoviesFromDirector(array, director) {
+  var result = array.filter(function (movie) {
+    return movie.director === director;
+  });
+  console.log("EXERCICE 2 ->", result);
+  return result;
+}
+
+// Exercise 3: Calculate the average of the films of a given director.
+function moviesAverageOfDirector(array, director) {}
+
+// Exercise 4:  Alphabetic order by title 
+function orderAlphabetically(array) {}
+
+// Exercise 5: Order by year, ascending
+function orderByYear() {}
+
+// Exercise 6: Calculate the average of the movies in a category
+function moviesAverageByCategory() {}
+
+// Exercise 7: Modify the duration of movies to minutes
+function hoursToMinutes() {}
+
+// Exercise 8: Get the best film of a year
+function bestFilmOfYear() {}
+
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
+if (typeof module !== 'undefined') {
+  module.exports = {
+    getAllDirectors: getAllDirectors,
+    getMoviesFromDirector: getMoviesFromDirector,
+    moviesAverageOfDirector: moviesAverageOfDirector,
+    orderAlphabetically: orderAlphabetically,
+    orderByYear: orderByYear,
+    moviesAverageByCategory: moviesAverageByCategory,
+    hoursToMinutes: hoursToMinutes,
+    bestFilmOfYear: bestFilmOfYear
+  };
+}
